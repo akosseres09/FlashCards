1c19d4141cd0f4890d6fee9fec97708a57e2d4f3
akosseres09 <akosseres75@gmail.com>
2025-12-12 14:17:41 +0100

    feat: Added routes to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'auth/login',
+    },
+    {
+        path: 'auth',
+        loadComponent: () => import('./layouts/auth/auth.component').then((m) => m.AuthComponent),
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'login',
+            },
+            {
+                path: 'login',
+                loadComponent: () =>
+                    import('./auth/login/login.component').then((m) => m.LoginComponent),
+            },
+            {
+                path: 'signup',
+                loadComponent: () =>
+                    import('./auth/signup/signup.component').then((m) => m.SignupComponent),
+            },
+            {
+                path: 'not-found',
+                loadComponent: () =>
+                    import('./auth/not-found/not-found.component').then((m) => m.NotFoundComponent),
+            },
+            {
+                path: '**',
+                redirectTo: 'not-found',
+            },
+        ],
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    standalone: true,
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('FlashCards');
+    protected readonly title = signal('FlashCards');
 }
