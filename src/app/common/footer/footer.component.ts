243dce36484b347d1048eab15baa1eccf5855352
akosseres09 <akosseres75@gmail.com>
2025-12-12 18:00:09 +0100

    feat: fixing ui issues

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LucideAngularModule } from "lucide-angular";
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
     selector: 'app-footer',
--- src/app/layouts/auth/auth.component.ts
+++ src/app/layouts/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../common/navbar/navbar.component";
+import { FooterComponent } from "../../common/footer/footer.component";
 
 @Component({
     selector: 'app-auth',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './auth.component.html',
     styleUrl: './auth.component.scss',
 })
