b595637f850c8fdbb8d82f0da0665f9eddf4a5ce
akosseres09 <akosseres75@gmail.com>
2025-12-15 18:32:16 +0100

    feat: added footer to inner pages

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../common/navbar/navbar.component';
+import { FooterComponent } from '../../common/footer/footer.component';
 
 @Component({
     selector: 'app-main',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss',
 })
