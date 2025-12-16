00daa43fcc19f7bd70b6e5f0236d57658bae71ce
akosseres09 <akosseres75@gmail.com>
2025-12-16 16:59:46 +0100

    feat: fix toast duration

--- src/app/main/projects/questions-modal/questions-modal.component.ts
+++ src/app/main/projects/questions-modal/questions-modal.component.ts
@@ -207,7 +207,7 @@ export class QuestionsModalComponent implements OnChanges {
             this.toastService.show('Questions imported successfully.');
         } catch (error: any) {
             const message = error.message || 'No questions imported! Invalid JSON format.';
-            this.toastService.show(message, 'error', 20000);
+            this.toastService.show(message, 'error');
         } finally {
             this.jsonForm.reset();
             this.isSaving = false;
